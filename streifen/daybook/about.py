9b5f78efd1133654e56663af868893a09cf7ac6e
Walter Gildersleeve <walter@gildersleeve.eu>
2018-06-05 09:38:32 +0200

    minor

--- streifen/daybook/about.py
+++ streifen/daybook/about.py
@@ -4,7 +4,7 @@ from streifen.daybook import load_api, DAYBOOK_ENV
 class AboutInfo(object):
     def __init__(self):
         self.payload = load_api("version")
-        self._motd = "Another change"
+        self._motd = "Default Message of the Day!"
 
     def header(self):
         return "Daybook: About"
