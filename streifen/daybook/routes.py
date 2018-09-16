ddde0a9381389eff61fb67aca97b56ec31143e82
Walter Gildersleeve <walter@gildersleeve.eu>
2018-09-16 21:55:24 +0200

    wrapping methods

--- streifen/daybook/routes.py
+++ streifen/daybook/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from ddtrace import tracer
 from streifen.daybook.dashboard import Dashboard
 from streifen.daybook.about import About
 
@@ -8,12 +9,14 @@ def _render(obj):
     template = obj.template_name()
     return render_template(template, **attrs)
 
+@tracer.wrap()
 @app.route('/')
 @app.route('/index')
 def dashboard():
     obj = Dashboard()
     return _render(obj)
 
+@tracer.wrap()
 @app.route('/about')
 def about():
     obj = About()
